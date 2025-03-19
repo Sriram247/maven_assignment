de0cfd469fd91bdf20ec1ed21bd1e0a18025012d
sriram <sriramachandran28@gmail.com>
2025-03-18 22:28:55 -0300

    changes

--- src/test/java/com/smu/mscda/MainTest.java
+++ src/test/java/com/smu/mscda/MainTest.java
@@ -1,38 +1,21 @@
 package com.smu.mscda;
+import static org.junit.Assert.assertEquals;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class MainTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MainTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MainTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class MainTest {
+@Test
+public void testCapitalizeString() {
+String input = "hello";
+String expected = "Hello";
+assertEquals(expected, StringUtils.capitalize(input));
+}
+@Test
+public void testMd5Hash() {
+String input = "Hello";
+String expectedMd5 = DigestUtils.md5Hex(input);
+assertEquals(expectedMd5, DigestUtils.md5Hex(input));
+}
 }
